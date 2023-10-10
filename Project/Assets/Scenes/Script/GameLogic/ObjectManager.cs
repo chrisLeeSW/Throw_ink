10ea457434deafa37ab372933d9e88fbd0bbbef8
이승우 <chrisnarulee123@gmail.com>
2023-10-10 13:52:25 +0900

    스테이지 자동움직이는 위치설정 작업 완료됨 현재 1개의 오브젝트만 움직이게 보기 쉽게 해둠

--- Project/Assets/Scenes/Script/GameLogic/ObjectManager.cs
+++ Project/Assets/Scenes/Script/GameLogic/ObjectManager.cs
@@ -1,21 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using System;
 
 public class ObjectManager : MonoBehaviour
 {
     public List<GameObject> colorObjectPrefab;
     private int currentColorObject;
 
-    public List<GameObject> makeColorObjectPrefab;
+    private List<GameObject> makeColorObjectPrefab;
+    private int currentMakeColorObjectpreFab;
 
     public List<Transform> objectPosition;
     private int currentobjectPosition;
 
     public List<Transform> spwanPosition;
     private int currentSpwanPosition;
-    
-    
+
+
+    private Vector3 objDirection;
+    private Quaternion startRot;
+    private Quaternion endRot;
+
+    private float startTime;
+    private float newTimeSpeed;
+    public float moveSpeed = 3f;
+    float roate;
+    private float journeyLength;
+    public float rotateSpeed = 0.25f;
+    private Vector3 startMovePosition;
+    private void Awake()
+    {
+        makeColorObjectPrefab =new List<GameObject>();
+
+    }
     void Update()
     {
         if(Input.GetKeyUp(KeyCode.Alpha1)) 
@@ -28,7 +47,9 @@ public class ObjectManager : MonoBehaviour
             MakeColorObject();
 
         if (Input.GetKeyDown(KeyCode.Alpha0))
-            MoveColorObject();
+        {
+            DirectionSettingColorObject();
+        }
     }
 
     void ChangeColorObject()
@@ -49,12 +70,43 @@ public class ObjectManager : MonoBehaviour
 
     void MakeColorObject()
     {
-        var makingObj = Instantiate(colorObjectPrefab[currentColorObject], spwanPosition[currentSpwanPosition]);
+        var makingObj = Instantiate(colorObjectPrefab[currentColorObject]);
+        makingObj.transform.position = spwanPosition[currentSpwanPosition].transform.position;
         makeColorObjectPrefab.Add(makingObj);
     }
 
-    void MoveColorObject()
+    void DirectionSettingColorObject()
     {
+        objDirection = makeColorObjectPrefab[currentMakeColorObjectpreFab].transform.position - objectPosition[currentobjectPosition].transform.position ;
+        objDirection.Normalize();
+        startRot = makeColorObjectPrefab[currentMakeColorObjectpreFab].transform.rotation;
+        endRot = Quaternion.LookRotation(objDirection);
+        startMovePosition = makeColorObjectPrefab[currentMakeColorObjectpreFab].transform.position;
+        startTime = Time.time;
 
+        journeyLength = Vector3.Distance(makeColorObjectPrefab[currentMakeColorObjectpreFab].transform.position, objectPosition[currentobjectPosition].transform.position);
+
+        StartCoroutine(MoveColorObject());
+    }
+
+    IEnumerator MoveColorObject()
+    {
+        while(true)
+        {
+            yield return null;
+            newTimeSpeed = (Time.time - startTime) * moveSpeed;
+            roate = newTimeSpeed / journeyLength;
+            makeColorObjectPrefab[currentMakeColorObjectpreFab].transform.rotation = Quaternion.Slerp(startRot, endRot, roate * rotateSpeed);
+            makeColorObjectPrefab[currentMakeColorObjectpreFab].transform.position = Vector3.Lerp(startMovePosition, objectPosition[currentobjectPosition].transform.position, roate);
+
+            if (Vector3.Distance(makeColorObjectPrefab[currentMakeColorObjectpreFab].transform.position, objectPosition[currentobjectPosition].transform.position) < 0.1f)
+            {
+                currentobjectPosition++;
+                if (currentobjectPosition >= objectPosition.Count)
+                    currentobjectPosition = 0;
+                break;
+            }
+        }
+        
     }
 }
